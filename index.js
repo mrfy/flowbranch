c2fa41f2a7d6bab0b705da11984f1844dd106a48
Grzegorz Fydrych <grzegorz.fydrych@grandvision.com>
2021-07-07 08:53:32 +0200

    comment

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello World\n");
 });
+//comment
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
